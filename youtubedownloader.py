f14f4a11376b1af207419182f4a45f34fd9058cf
shahdbedair <84231867+shahdbedair@users.noreply.github.com>
2021-05-28 20:54:02 +0200

    Update youtubedownloader.py

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -5,44 +5,44 @@ link = input("please enter the video url :")
 
 video = YouTube(link)
 
-#print(f"the video title is:\n{video.title}\n")
-#print(f"the video description is:\n{video.description}\n")
-#print(f"the video views are:\n{video.views}\n")
-#print(f"the video rating is:\n{video.rating}\n")
-#print(f"the video duration is:\n{video.length/60} minutes\n")
+print(f"the video title is:\n{video.title}\n")
+print(f"the video description is:\n{video.description}\n")
+print(f"the video views are:\n{video.views}\n")
+print(f"the video rating is:\n{video.rating}\n")
+print(f"the video duration is:\n{video.length/60} minutes\n")
 
-#print(video.streams)
+print(video.streams)
 
-#for stream in video.streams:
-   # print(stream)
+for stream in video.streams:
+    print(stream)
 
-#for stream in video.streams.filter(progressive=False):
-  #  print(stream)    
+for stream in video.streams.filter(progressive=False):
+    print(stream)    
 
-#for stream in video.streams.filter(progressive=True,res="720p"):
-    #print(stream)
+for stream in video.streams.filter(progressive=True,res="720p"):
+    print(stream)
 
-#for stream in video.streams.filter(subtype='mp4'):
-  #  print(stream)    
+for stream in video.streams.filter(subtype='mp4'):
+    print(stream)    
 
-#for stream in video.streams.filter(res='10'):
- #   print(stream)  
+for stream in video.streams.filter(res='10'):
+    print(stream)  
 
-#print(video.streams.get_highest_resolution())   
+print(video.streams.get_highest_resolution())   
 
-#print(video.streams.get_lowest_resolution())
+print(video.streams.get_lowest_resolution())
 
 def finish():
   print("download done ")
 
-video.streams.get_highest_resolution().download(output_path="E:\Documents")
+video.streams.get_highest_resolution().download(output_path="the place you want videos to save in")
 video.register_on_complete_callback(finish())
 
-#from pytube import Playlist
+from pytube import Playlist
 
-#Playlist_link = input("please enter the playlist link :")
-#Playlist = Playlist(Playlist_link)
+Playlist_link = input("please enter the playlist link :")
+Playlist = Playlist(Playlist_link)
 
-#for video in Playlist.videos:
-  #  video.streams.get_highest_resolution().download(output_path="E:")
- #   video.register_on_complete_callback(finish())
+for video in Playlist.videos:
+    video.streams.get_highest_resolution().download(output_path="the place you want videos to save in")
+    video.register_on_complete_callback(finish())
